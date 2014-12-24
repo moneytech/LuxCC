91ec88d1b74f505f427dec5c9df47f86f9acd008
lugon <clrg@clrg-300E5A.(none)>
2014-12-23 21:46:48 -0300

    decl ast nodes

--- src/parser.h
+++ src/parser.h
@@ -24,6 +24,56 @@ struct Declaration {
 };
 #endif
 
+typedef struct TypeExp TypeExp;
+typedef struct Declaration Declaration;
+typedef struct DeclList DeclList;
+typedef struct FuncDef FuncDef;
+typedef struct ExternDecl ExternDecl;
+typedef struct ExecNode ExecNode;
+
+struct ExecNode {
+    int xyz;
+};
+
+struct TypeExp {
+    Token op;
+    char *str;
+    union {
+        DeclList *dl;
+        ExecNode *e;
+    } attr;
+    TypeExp *child, *sibling;
+};
+
+struct Declaration {
+    TypeExp *decl_specs;
+    TypeExp *idl;
+};
+
+struct DeclList {
+    Declaration *decl;
+    DeclList *next;
+};
+
+struct FuncDef {
+    TypeExp *decl_specs;
+    TypeExp *header; /* declarator */
+    ExecNode *body;
+};
+
+typedef enum {
+    DECLARATION,
+    FUNCTION_DEFINITION
+} EDKind;
+
+struct ExternDecl {
+    EDKind kind;
+    union {
+        Declaration d;
+        FuncDef f;
+    } ed;
+};
+
 void parser(TokenNode *);
 
 #endif
