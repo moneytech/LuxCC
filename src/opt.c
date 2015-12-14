adfcb2234884311e7c450a9251d5eb46ce9b461e
lugon <lugon399@gmail.com>
2015-12-14 00:45:55 -0300

    Add opt.h/opt.c.

---
+++ src/opt.c
@@ -0,0 +1,35 @@
+#define DEBUG 0
+#include "opt.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "util.h"
+#include "ic.h"
+#include "expr.h"
+#include "bset.h"
+
+void opt_main(void)
+{
+    unsigned n1;
+
+    for (n1 = 0; n1 < cg_nodes_counter; n1++) {
+        unsigned n2;
+
+        for (n2 = cg_node(n1).bb_i; n2 <= cg_node(n1).bb_f; n2++) {
+            unsigned i;
+
+            for (i = cfg_node(n2).leader; i <= cfg_node(n2).last; i++) {
+                unsigned tar, arg1, arg2;
+
+                tar = instruction(i).tar;
+                arg1 = instruction(i).arg1;
+                arg2 = instruction(i).arg2;
+
+                switch (instruction(i).op) {
+                    ;
+                }
+            }
+        }
+    }
+}
---
+++ src/opt.h
@@ -0,0 +1,6 @@
+#ifndef OPT_H_
+#define OPT_H_
+
+void opt_main(void);
+
+#endif
