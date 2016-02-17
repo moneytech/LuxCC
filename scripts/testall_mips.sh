e60f922554a385d536180886a0a0018a453b9b26
lugon <lugon399@gmail.com>
2016-02-17 16:28:54 -0300

    Fix typo in path.

--- scripts/testall_mips.sh
+++ scripts/testall_mips.sh
@@ -5,7 +5,7 @@ echo " START MIPS TESTS"
 echo "============================="
 
 # assume default installation paths
-if [ ! -d "$HOME/mgc/embedded/codebench/mips-linux-gnux/" ] ; then
+if [ ! -d "$HOME/mgc/embedded/codebench/mips-linux-gnu/" ] ; then
 	echo "cross-development environment not found."
 else
 	scripts/self_mips.sh &&
