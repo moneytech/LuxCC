1a7a06e0174b472ab1651109b24709b2b3874fe2
lugon <lugon399@gmail.com>
2016-03-22 23:20:31 -0300

    Add ar.h header.

---
+++ src/lib/include/ar.h
@@ -0,0 +1,18 @@
+#ifndef _AR_H
+#define _AR_H
+
+#define ARMAG   "!<arch>\n" /* magic string */
+#define SARMAG  8           /* length of magic string */
+#define ARFMAG  "`\n"       /* header trailer string */
+
+struct ar_hdr {             /* file member header */
+    char    ar_name[16];    /* '/' terminated file member name */
+    char    ar_date[12];    /* file member date */
+    char    ar_uid[6]       /* file member user identification */
+    char    ar_gid[6]       /* file member group identification */
+    char    ar_mode[8]      /* file member mode (octal) */
+    char    ar_size[10];    /* file member size */
+    char    ar_fmag[2];     /* header trailer string */
+};
+
+#endif /* ar.h */
