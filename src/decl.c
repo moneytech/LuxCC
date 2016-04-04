moneytech/LuxCC

commit 0aa7d7913f66c775e8667c9101ec3cbacf63f6e3

Fix bug related to aggregates and declarations that was causing segfaults.

src/decl.c
@@ -1503,6 +1503,9 @@ void analyze_init_declarator(TypeExp *decl_specs, TypeExp *declarator, int is_fu
             /* update previous declaration */
             if (is_initialized || is_func_def)
                 prev->declarator = declarator;
+            if (is_initialized)
+                /* analyze_initializer() sets attr.dl (the declaration list) for struct and unions */
+                prev->decl_specs = decl_specs;
         }
     } else {
         /*

src/tests/execute/aggregates_and_extern.c
@@ -1,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct A A;
+
+struct A {
+    int x;
+    int y;
+};
+
+extern A g;         /* install_external_id() is called */
+A g = { 10, 20 };   /* analyze_init_declarator() should update the
+                       previously installed entry */
+
+int main(void)
+{
+    printf("%d %d\n", g.x, g.y);
+    return 0;
+}

src/x86_cgen/x86_cgen.c
@@ -2692,6 +2692,7 @@ void x86_static_init(TypeExp *ds, TypeExp *dct, ExecNode *e)
 
         /* handle members with explicit initializer */
         d = ts->attr.dl;
+        assert(d != NULL);
         full_init = FALSE;
         for (; d != NULL; d = d->next) {
             dct = d->decl->idl;

commit 1ca07ab06edf5d17683ffa80ad2c804b1267ca60

Fix bug introduced by 0aa7d79.

src/decl.c
@@ -1503,9 +1503,15 @@ void analyze_init_declarator(TypeExp *decl_specs, TypeExp *declarator, int is_fu
             /* update previous declaration */
             if (is_initialized || is_func_def)
                 prev->declarator = declarator;
-            if (is_initialized)
+            if (is_initialized) {
+                TypeExp *pts;
+
                 /* analyze_initializer() sets attr.dl (the declaration list) for struct and unions */
-                prev->decl_specs = decl_specs;
+                if (declarator->child == NULL
+                && ((pts=get_type_spec(prev->decl_specs))->op==TOK_STRUCT || pts->op==TOK_UNION)
+                && pts->attr.dl == NULL)
+                    pts->attr.dl = get_type_spec(decl_specs)->attr.dl;
+            }
         }
     } else {
         /*